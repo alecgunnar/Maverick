85ed822a44f0df82e4d434b74c22d315085034a4
Alec Carpenter <gunnar94@me.com>
2013-08-30 09:14:00 -0400

    Removed my personal email address from the config file

--- Config/Master/System.php
+++ Config/Master/System.php
@@ -7,4 +7,4 @@
 return array('site_name'    => 'New Website',
              'description'  => 'A new website',
              'admin_name'   => 'Administrator',
-             'admin_email'  => 'gunnar94@me.com');
+             'admin_email'  => 'admin@website.com');
