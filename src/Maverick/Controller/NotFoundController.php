ace6c2ed67fcac9cc279d72091b3a98a3aa6bb4c
gunnar94 <gunnar94@me.com>
2015-08-25 08:42:34 -0400

    Added not found controller

---
+++ src/Maverick/Controller/NotFoundController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Maverick
+ *
+ * @author Alec Carpenter <gunnar94@me.com>
+ */
+
+namespace Maverick\Controller;
+
+class NotFoundController implements ControllerInterface
+{
+    public function doAction()
+    {
+        
+    }
+}
