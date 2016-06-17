3921d7885f5c5b23d97e51c638c5be733a3a0622
alecgunnar <alecgunnar@gmail.com>
2016-06-17 11:48:22 -0400

    Added middleware interface

---
+++ src/Middleware/MiddlewareInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Maverick Container
+ *
+ * @author Alec Carpenter <alecgunnar@gmail.com>
+ */
+
+namespace Maverick\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+interface MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @param callable $next
+     * @return ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface;
+}
