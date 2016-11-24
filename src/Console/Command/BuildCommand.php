31664d25a594506f8f5aa4d2c851bd715e3f3cd6
alecgunnar <alecgunnar@gmail.com>
2016-11-24 11:26:56 -0500

    Updated script name

--- src/Console/Command/BuildCommand.php
+++ src/Console/Command/BuildCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CreateUserCommand extends Command
+class BuildCommand extends Command
 {
     protected function configure()
     {
