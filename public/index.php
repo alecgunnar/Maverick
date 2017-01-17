e47627bfb771de785c71edf358f6fc5f6ff44250
ACarpenter <aleccarpenter@quickenloans.com>
2017-01-17 09:44:37 -0500

    Removed public from gitignore

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+
+use GuzzleHttp\Psr7\ServerRequest;
+
+// Define some paths and files
+
+$root = dirname(__DIR__);
+$autoloader = $root . '/vendor/autoload.php';
+$cache = $root . '/cache';
+
+// Check for the autoloader
+
+if (!file_exists($autoloader)) {
+    die('Please run `composer install` before atteming to load this example.');
+}
+
+// Get the autoloader
+
+require $autoloader;
+
+// Create the cache directory  if it does not already exist
+
+if (!is_dir($cache)) {
+    mkdir($cache);
+}
+
+// Load the container and the application from it
+
+$container = \Maverick\bootstrap($root, true);
+$app = $container->get('application');
+
+// Run the application
+
+$response = $app->handleRequest(ServerRequest::fromGlobals());
+$app->sendResponse($response);
